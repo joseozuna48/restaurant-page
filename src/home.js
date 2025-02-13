a724002b23d8e38915970c02ac7d7c6b24d48d63
Jose Ozuna <joseozuna48@gmail.com>
2025-02-12 20:46:55 -0700

    Add Home module and adjust styles

---
+++ src/home.js
@@ -0,0 +1,46 @@
+export function loadHome(){
+
+    // set tab as active
+    let buttons = document.querySelectorAll("nav button");
+    buttons.forEach( (button)=> {
+        button.classList.remove("active");
+        if(button.classList.contains("home") ) button.classList.add("active");
+    }  );
+
+
+    let schedArray = [ "Sunday: 8am - 8pm","Monday: 6am - 6pm","Tuesday: 6am - 6pm","Wednesday: 6am - 6pm","Thursday: 6am - 10pm","Friday: 6am - 10pm","Saturday: 8am - 10pm"];
+    let container = document.getElementById("content");
+
+
+    // First Block Section
+    let resName = document.createElement("h1");
+    resName.textContent = "Restaurant Name";
+
+    // Second Block section
+    let mainText = document.createElement("p");
+    mainText.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam doloribus quaerat odio ut, facilis, magnam ipsum ullam, iusto doloremque cupiditate voluptatum iste tenetur nesciunt perferendis temporibus quis modi excepturi ipsam!";
+
+    container.appendChild(resName);
+    container.appendChild(mainText);
+
+    // third block section
+    let schedule = document.createElement("div");
+    schedule.classList.add("schedule");
+
+    let schedHeader = document.createElement("h2");
+    schedHeader.textContent = "Hours";
+
+    let hours = document.createElement("ul");
+    schedArray.forEach(element => {
+        let hourElement = document.createElement("li");
+        hourElement.innerText = element;
+
+        hours.appendChild(hourElement);
+    });
+
+    schedule.appendChild(schedHeader);
+    schedule.appendChild(hours);
+
+    container.appendChild(schedule);
+
+}
